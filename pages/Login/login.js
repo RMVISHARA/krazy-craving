b38a35b681d4297dae1880b67db6824d4eba62c4
Udara <udaranayan.111@gmail.com>
2022-01-11 14:34:27 +0530

    js changes in login and landing pages

--- pages/Login/login.js
+++ pages/Login/login.js
@@ -1,13 +1,19 @@
 function Login() {
+    var invalidLogins = true;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    if (email == "user@gmail.com" && password == "123") {
-        localStorage.setItem("totalPoint", 30)
+    var configs = [["viduthishara@gmail.com","123"],["udaranayana.111@gmail.com","123"],["user@gmail.com","123"]];
+    configs.forEach( e => {
+        if (email == e[0] && password == e[1]) {
+            localStorage.setItem("totalPoint", 30)
             //console.log(localStorage.getItem("totalPoint"))
-        window.location.href = "../HomeUser"
-    } else if (email == "vendor@gmail.com" && password == "456") {
-        window.location.href = "../VendorOrders/newOrder.html"
-    } else {
+            invalidLogins = false;
+            window.location.href = "../HomeUser"
+        }
+    });
+
+    if(invalidLogins){
         alert("Invalid username and password");
     }
+    
 }
